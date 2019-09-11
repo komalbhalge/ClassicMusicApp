a8ad941076af9b8b0632d4a1b8d24cf335eb862f
Komal <komal.bhalge@yahoo.com>
2019-09-11 23:37:11 +0800

    Added comment.

--- app/src/main/java/com/example/classicmusic/utils/Constants.java
+++ app/src/main/java/com/example/classicmusic/utils/Constants.java
@@ -1,7 +1,7 @@
 package com.example.classicmusic.utils;
 
 public class Constants {
-
+    /*All Projects constants*/
     public static final String TAG= "KTag";
     public static final String LAUNCHED_FROM_NOTIFICATION= "LaunchedFromNotification";
 
